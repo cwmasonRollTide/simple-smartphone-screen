4fe82a9effa400e3cceae943cc1fdcf1bd30e428
Connor Mason <cdubyamason@outlook.com>
2023-03-06 15:58:38 -0500

    adding in the image for the person you are texting

--- src/components/MessageDisplay/index.js
+++ src/components/MessageDisplay/index.js
@@ -1,20 +1,18 @@
+import React from 'react';
 import './MessageDisplay.css';
 import TextPost from "../TextPost";
 import ImagePost from "../ImagePost";
-import React, {useEffect, useState} from 'react';
 
 const MessageDisplay = ({messages}) => {
   return (
-    <div className="smartphone-container">
-      <div className="smartphone-conversation">
-        {messages.map((message, index) => {
-          if (!message.mediaUrl0) {
-            return <TextPost message={message.messageBody} index={index} />
-          } else {
-            return <ImagePost messageBody={message.messageBody} mediaUrl={message.mediaUrl0} />
-          }
-        })}
-      </div>
+    <div className="smartphone-conversation">
+      {messages.map((message, index) => {
+        if (!message.image) {
+          return <TextPost message={message.messageBody} index={index} />;
+        } else {
+          return <ImagePost messageBody={message.messageBody} mediaUrl={message.image} />;
+        }
+      })}
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import "./SimpleSmartphoneComponent.css";
 import MessageDisplay from "./MessageDisplay";
 
-const SimpleSmartphoneComponent = ({messages}) => {
+const SimpleSmartphoneComponent = (messages, mainImageUrl) => {
   return (
-    <div className="App">
-      <MessageDisplay messages={messages} />
+    <div className="component">
+      <div className="smartphone-container">
+        <div className="main-image-container">
+          <img src={mainImageUrl} className="main-image"/>
+        </div>
+        <MessageDisplay messages={messages} />
+      </div>
     </div>
   );
 };
 
+SimpleSmartphoneComponent.propTypes = {
+  messages: PropTypes.arrayOf({
+    image: PropTypes.string,
+    messageBody: PropTypes.string.isRequired
+  }),
+  mainImageUrl: PropTypes.string
+};
 export default SimpleSmartphoneComponent;
+
