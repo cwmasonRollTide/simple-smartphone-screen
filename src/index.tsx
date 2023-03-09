9a8c39ae489957231f30bbed3d1e81801da0c044
Connor Mason <cdubyamason@outlook.com>
2023-03-08 22:13:38 -0500

    no dist folder

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import Example from "./examples";
 import ReactDOM from 'react-dom/client';
 import Message from "./interfaces/Message";
 import SmartphoneProps from "./interfaces/SimpleSmartphoneComponentProps";
+import SimpleSmartphoneComponent from 'components';
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(<Example />);
 
-export { Message, SmartphoneProps};
+export { Message, SmartphoneProps, SimpleSmartphoneComponent};
 
