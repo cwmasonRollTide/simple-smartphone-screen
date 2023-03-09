e0126f88276111133d5b99e59119de2426e5911e
Connor Mason <cdubyamason@outlook.com>
2023-03-08 22:05:39 -0500

    lets see if I fixed the build error I introduced

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Example from "./examples";
 import ReactDOM from 'react-dom/client';
+import Message from "./interfaces/Message";
+import SmartphoneProps from "./interfaces/SimpleSmartphoneComponentProps";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(<Example />);
+
+export { Message, SmartphoneProps};
+
